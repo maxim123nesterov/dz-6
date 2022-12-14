3a2aa522f59b2caad5f827cf46aa7b16eab0d1ae
Maxim <maxmaxmaxmaxmaxa@mail.ru>
2022-12-15 01:57:34 +0300

    task 41

---
+++ task 41/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите размер массива - ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[n];
+int i = 0;
+int k = 0;
+Console.WriteLine();
+for(i = 0; i < arr.Length; i++)
+{
+Console.Write("Введите число в массив - ");
+arr[i] = Convert.ToInt32(Console.ReadLine());
+if (arr[i] > 0)
+{
+    
+    k = k + 1;
+    
+}
+
+}
+Console.WriteLine();
+Console.WriteLine($"Количество чисел больше 0 = {k}");
